41514a5ed23dce2b2001723dc01f5b379e6d820d
somchai-cedt <6733266021@student.chula.ac.th>
2025-03-02 11:51:53 +0700

    Security

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
+const mongoSanitize = require("express-mongo-sanitize");
+const {xss} = require("express-xss-sanitizer");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
 
 dotenv.config({path: "./config/config.env"});
 
@@ -11,6 +16,15 @@ const authRouter = require("./routes/auth");
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(mongoSanitize());
+app.use(xss());
+const limiter=rateLimit({
+    windowsMs:10*60*1000,//10 mins
+    max: 100
+});
+app.use(limiter);
+app.use(hpp());
+app.use(cors());
 
 app.use("/api/v1/dentists", dentistsRouter);
 app.use("/api/v1/bookings", booksRouter);
